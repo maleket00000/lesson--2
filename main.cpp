8cc506f8feaa74e3317acf8fa5c3c10e44eeb959
maleket00000 <82824359+maleket00000@users.noreply.github.com>
2021-04-19 20:40:10 +0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main()
+{
+    cout<< "ra styva gainteresebs Nikolooo?"<<endl;
+    string x, result;
+    while (x = "TED", "unicorn") {
+    getline(cin, x);
+    if (x == "TED") { result = "**** the acronym TED means: technology, entertainment, and design.ed";
+    } else if (x== "new media") {result = "****means of mass communication using digital technologies such as the Internet.";
+    } else if (x== "build relationships") {result = "****create and stenghten relationships with other people";
+    } else if (x== "head up")  {result = "****to lead or control a group, part of a company, etc";
+    } else if (x== "startup") {result = "****a small business that has just been started.";
+    } else if (x== "embed") {result = "****to fix something firmly into a substance(chasma)";
+    } else if (x== "temporarily") {result = "**** in limited time only";
+    } else if (x== "URL") { result = "****Uniform Resource Location- The internet address for the website";
+    } else if (x== "bummer") {result = "****something that is very annoying or not convenient      ";
+    } else if (x== "default page") {result = "****the page which doesn't exist";
+    } else if (x== "inherently") {result = "****in permanent, essential, or characteristic way;";
+    } else if (x== "hit") {result = "****to come across";
+    } else if (x== "fall through") { result = "****if something that has been planned or agreed falls trough, it doesn't happen.";
+    } else if (x== "unicorn") {result = "****an imaginary white creature like horse with a single horn frowing from the front of itshad";
+    } else if (x== "comes into play") { result = "****starts action, cames into the action";
+    } else if (x== "technology incubator") { result = "****a company htah helps startups to develop";
+    } else if (x== "a light bulb does in ab's head ") { result = "****used to say that a person suddenly understands something or has a great idea; After thinking about the problem for a several days";
+    } else if (x== "commiserate") {result = "feel or express pity for someone; sympathize";
+    } else if (x== "hound") {result = "****a type of dog used for hunting";
+    } else if (x== "slap in the face") {result = "****action or criticism that is unkind and that makes you feel sad or disappointed.\n if you describe something that someone does as a slap in the face,\n you mean that it shocks or upsets you because it shows that they do not support you or respect you";
+    } else if (x== "crack") {result ="****a line on the surface pof something along which it has split without breaking apart";
+    } else if (x== "deal with") {result ="**** 1) to have something to do. 2) to take action in roder to /n achieve something or in order to solve a problem";
+    } else if (x== "fall through the cracks") {result = "****not be noticed or dealt with:little details often fall through the clack";
+    } else if (x== "a slap in the face") {result = "****if you desctibe something that someone does as a slap in the face,\n you mean that it shocks or upsets you because it shows \n that they do not support you or respect.";
+    } else if (x== "figure out") {result = "****to understand or solve somethibg";
+    } else if (x== "doodle") {result = "****a drawing or pattern that you make while thinking about something else or when you are bored";
+    } else if (x== "google doodle") {result = "****the different variations of the logo on the google home page that company uses for a seasons\n or special days suck as birthdays of famous people ";
+    } else if (x== "relate to something") {result = "****understand it because you have experienced the same thing or something similar";
+    } else if (x== "viral video") {result = "****a video that becomes popular because people watch it and share it with others who, in turn, share\n it with even more people, creating a viral effect.";
+    } else if (x== "infographic") {result =" ****a picture or desig or a group of picture or diagrams showing or explaining information in visual form";
+    } else if (x== "online content") {result = "****any material available on the Web. online content includes text,\nimages, animations, musics and videos.";
+    } else if (x== "trend") {result = "**** 1) a general direction in which something is developing or charging.\n 2) a fasion";
+    } else if (x== "course 101") {result = "**** comprising(contains) the introductory material in a course of study.\n EX: in american university course numbering systems,\nthe number 101 is often used for an introductory course at a beginner's level in a department's subject area";
+    } else if (x== "stream") {result= "**** to listen or watch sound or video on a computer, mobile phone , etc. \ndirectly from the internet rather than downloading it and saving it first, or to send sound\n or video in this way: stream video/music/files.";
+    } else if (x== "broadcasting") {result = "****transmit(a programme or some inormation) by radio or television";
+    } else if (x== "track") {result = "****follow the trial or movements of(someone or something),typically in order to find them or note their course. \n synonyms:follow , chase, track";
+    } else if (x== "short-listed") {result = "**** 'to shortlist' means you choose the better ones from the applications or candidates and from \na shorter list of names. from the shorter list of a few names, a final winner will be selected. If you are shortlisted,\nit means you have succeeded in the initial rounds of selection";
+    } else if (x== "host") {result = "****to store(a website or other data) on a server orother computer so tht it can be accessed over the Internet";
+    } else if (x== "networker") {result = "**** 1) a person who works from home or from an external office via a computer network \n2) comeone who tries to meet people who might be useful to know, especially in their job.";
+    } else if (x== "chunk") {result = "****large part of something";
+    } else if (x== "out of reach") {result = "**** not able to be achieved";
+    } else if (x== "ICT") {result = "****Information, Communication and Technology";
+    } else if (x== "literacy") {result = "****the ability to read or write or the ability to use something , e.g. technology, as a tool";
+    } else if (x== "personal branding") {result = "****the process of creating a recognizable professional\nname and reputation for yourself(or your company/business).\n It is a combination of how you present yourself through the various means\nof communication and of how clients remember youo after you have provided the serve.";
+    } else if (x== "acupuncture") {result = "****a treatment for pain or illness in which thin needles\n are positioned just under the surface of the skin ar\nspecial points around the body";
+    } else if (x== "personal perspective") {result = "****personal outlook/viewpoint";
+    } else if (x== "passionate") {result = "****having very strong feelings or emotions";
+    } else if (x== "committed") {result = "****synonyms: devoted, faithful, pledged";
+    } else if (x== "co-ordinate") {result = "****to organize all the different activities and people who are involved in something that it works effectiveli";
+    } else if (x== "follow-up") {result= "****a continuation or representation of something that has already been started or done";
+    } else if (x== "presence") {result = "****being seen or noticed in a place (here the place is the Internet)";
+    } else if (x== "at ease") {result = "**** if you are at ease, you are feeling confident and relaxed, and are able to talk\nto people without feeling nerwous or anxious";
+    } else if (x== "vision") {result = "****an idea or imagine in your mind";
+    } else if (x== "apparently") {result = "**** 1) obviously 2) seemiingly, evidently";
+    } else if (x== "self-promotion") {result = "****the action of promoting or publicizing oneself or one's activities";
+    } else if (x== "empathy") {result = "*****the ability to understand and share the feeling s of another";
+    } else if (x== "expertise") {result = "****expert skill or knowledge in particular field. (something like -to have experience)";
+    } else if (x== "session") {result = "****time of some introducing activitie(GE: 'sesia')";
+    } else if (x== "self-image") {result = "**** is a mental picture of yourself, both as a physical body and an individual.";
+    } else if (x== "all-important") {result = "****vitally important; cruciall";
+    } else if (x== "reflexology") {result = "**** the practice of massaging practicular areas of the body, especially the feet,\nin the belief that it can heal particular organs";
+    } else if (x== "complementary medicine") {result = "****alternative medicine, not tradicional medicine";
+    } else if (x== "archive") {result = "****a collection of historical documents or records providing information\n about a place, institution or group of people";
+    } else if (x== "modest") {result = "****relatively moderate, limited or small";
+    } else if (x== "to come up with") {result= "****to produce; to suggest or think of an idea or plan";
+    } else if (x== "greckon") {result = "****consider, establish by calculation";
+    } else if (x== "networking") {result = "****a group or system of  interconected people or things";
+    } else if (x== "company-wide") {result = "****relating to or affecting the whole company";
+    } else if (x== "neighbour association") {result = "****an organisation composed of voluntary living in a particular neighbourhood, which aims\nto improve the social and communal facilities of the neighbourhood \nand to conserve or improve its environmental advantages.";
+    } else if (x== "mutual acquintance") {result = "****the person who is familar for other two , three etc.";
+    } else if (x== "open-ended question") {result = "****a question inwhich(unlike in a multiple choice question) possible answer are not suggested,\nand the respodent answers it in his or her own words";
+    } else if (x== "identity fraud") {result = "****when thief uses someones passport of bank details for their own financial benefit;\nis when a thief uses stolen information to make money";
+    } else if (x== "sophisticated") {result = "****having, revealing, or proceeding from a great deal of worldly experience and knowledge of fashion and culture";
+    } else if (x== "deceive") {result = "****cause to believe something that is not true, in order to gain some personal advantage        ";
+    } else if (x== "identity theft") {result = "****when a person's private information is stolen";
+    } else if (x== "phishing") {result = "****when someone is tricked into revealing their personal informations";
+    }
+    else if (x== "stop process") {break;}
+      else {result = "Chemi yle!!!!!! arasworad wer sityvas!"; }
+    cout << result<<endl;}
+return 0;
+}
